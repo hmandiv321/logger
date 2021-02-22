c63c06e3b7acfe8d876a007c38b0c062fc2ef043
HR Mandiv <51318320+hmandiv321@users.noreply.github.com>
2021-02-21 23:24:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
 const logger = require('./logger');
-const { fgRed, fgGreen, fgYellow, fgCyan, fgWhite } = require('./styles');
 
-module.exports = {
-  /**
-   * @description logs to the console in red colored text
-   */
-  error: (...data) => logger(fgRed, ...data),
-  /**
-   * @description logs to the console in green colored text
-   */
-  success: (...data) => logger(fgGreen, ...data),
-  /**
-   * @description logs to the console in yellow colored text
-   */
-  warning: (...data) => logger(fgYellow, ...data),
-  /**
-   * @description logs to the console in cyan colored text
-   */
-  info: (...data) => logger(fgCyan, ...data),
-  /**
-   * @description logs to the console in white colored text
-   */
-  log: (...data) => logger(fgWhite, ...data),
-};
+module.exports = logger;
